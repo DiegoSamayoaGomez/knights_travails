46d02a3c046d704312ec3ae2897e65308c92f150
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2025-07-13 21:01:46 -0600

    Set all possible movements in L shape of the knight

---
+++ script.js
@@ -0,0 +1,13 @@
+function knigthMoves(start, end) {
+  // Create an array of all possible movements (L patterns)
+  const directions = [
+    [-2, -1],
+    [-2, 1],
+    [-1, -2],
+    [-1, 2],
+    [1, -2],
+    [1, 2],
+    [2, -1],
+    [2, 1],
+  ];
+}
