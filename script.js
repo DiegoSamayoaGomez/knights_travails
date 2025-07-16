56035a3a672495b9d4572e23dcd6dd76de2d656a
DiegoSamayoaGomez <samayoasg2014@gmail.com>
2025-07-15 21:10:14 -0600

    Create queue for BFS and store visited values

--- script.js
+++ script.js
@@ -15,4 +15,25 @@ function knigthMoves(start, end) {
   const isValid = (row, column) => {
     return row >= 0 && row < 8 && col >= 0 && col < 8;
   };
+
+  // Create queue
+  const queue = [];
+  // Create set to store visited positions
+  const visited = new Set();
+
+  queue.push({ pos: start, path: [start] });
+  visited.add(start.toString());
+
+  // BFS loop
+  while (queue.length > 0) {
+    // Remove from the Q the next node
+    const { pos, path } = queue.shift();
+    const [row, col] = pos;
+
+    // If we havent reach the end position, return the path
+    if (row === end[0] && col === end[1]) {
+      //console.log("PATH", path);
+      return path;
+    }
+  }
 }
